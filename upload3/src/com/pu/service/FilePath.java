cfa56ca97f9bcdfe9fa0772efac02b3f8b5926a1
puwenjie <fast@fast-PC>
2016-11-15 12:29:49 +0800

    更新时间：2016年11月15日，创建了获得path的静态方法

---
+++ upload3/src/com/pu/service/FilePath.java
@@ -0,0 +1,27 @@
+package com.pu.service;
+
+import javax.servlet.http.HttpServletRequest;
+/*
+ * time:2016年11月15日
+ * author:pu
+ * 功能获取服务器中文件夹的路径，其中有两个函数，
+ * 1，一个函数名为getFilePathzip（）为获取压缩包下载路径
+ * 2，一个函数名为getFilepath（）为获取解压后文件夹路径，
+ * 
+ */
+   public class FilePath {
+	//获取压缩包下载路径
+	public static String  getFilePathZip(HttpServletRequest request){
+		String pahtZip=request.getSession().getServletContext().getRealPath("/")+"upload";
+		return pahtZip;
+		
+	}
+	//获取解压后的文件路径
+	
+	public static String   getFilepath(HttpServletRequest request){
+		return null;
+		
+	}
+	
+	
+}
