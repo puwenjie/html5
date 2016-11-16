bd469f12f158bac986e60642aa26cc19157be92e
puwenjie <fast@fast-PC>
2016-11-16 17:21:10 +0800

    做了部分修改，添加了预留图上传的函数

--- upload1/src/com/pu/service/SaveFilePath.java
+++ upload1/src/com/pu/service/SaveFilePath.java
@@ -1,5 +1,15 @@
 package com.pu.service;
-
+/*
+ * time:2016年11月16日
+ * aurthor:pu
+ *  功能:实现上传路径保存到数据库中，其中包括的参数有
+ *  String filename;预览文件的名称
+ *	String filepathzip;预览文件的下载地址，下载文件为zip或者rar
+ *	String filepathurl;预览文件的地址
+ *	String preimgurl;预览图的地址，如果默认预留图地址为 img/pre.jpg
+ *  Integer downtimes;省略，默认为0
+ * 
+ */
 import com.pu.dao.FilepathinformDAO;
 import com.pu.pojo.Filepathinform;
 
---
+++ upload1/src/com/pu/service/UpPreImg.java
@@ -0,0 +1,22 @@
+package com.pu.service;
+/*
+ *时间：2016年11月16日
+ *author:pu
+ *功能：
+ *1，实现预留图片的上传，
+ *2，保存预留图片地址到数据库， 
+ *3，获取图片 
+ */
+public class UpPreImg {
+	//从前台上传图片到服务器，并且保存图片地址到数据库
+	 public void uploadImg(){
+		 //.....
+		 
+	 }
+	
+   //获取和数据库中filename相匹配的图片地址，返回保存在数据库中的预留图片地址
+	 public  String getPreImgurl(String filename){
+		 //......
+		 return null;
+	 }
+}
